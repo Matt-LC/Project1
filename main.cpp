32db9d9b3db36e8c2305f41766295c43e9e79246
Matt-LC <1477664179@qq.com>
2023-06-12 19:29:21 +0800

    开发1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include "demo.h"
 using namespace std;
 
 int main() {
 
 	cout << "hello,world!!" << endl;
+	returnRes();
 	system("pause");
 	return EXIT_SUCCESS;
 }
